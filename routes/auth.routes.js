a055666a4ba2d3f501b15b2ffab60665b5da6ea0
WHL99 <xjv298347@gmail.com>
2022-07-22 11:19:11 +0200

    delete password rule

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -17,19 +17,6 @@ router.post('/signup', (req, res, next) => {
     return;
   }
 
-  // const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
-  // if (!emailRegex.test(email)) {
-  //   res.status(400).json({ message: 'Please provide a valid email address.' });
-  //   return;
-  // }
-
-  // Use regex to validate the password format
-  // const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
-  // if (!passwordRegex.test(password)) {
-  //   res.status(400).json({ message: 'Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.' });
-  //   return;
-  // }
-
   User.findOne({ email })
     .then((foundUser) => {
       if (foundUser) {
