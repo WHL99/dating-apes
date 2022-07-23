2fe31150e6c0471248ac598c04b48ae4555db3b5
WHL99 <xjv298347@gmail.com>
2022-07-23 13:54:19 +0200

    style walk-around and logout

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -10,21 +10,25 @@ function Navbar() {
     const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
 
     return (
-        <nav className='Navbar-1'>
+        <nav className='Navbar-0'>
 
-            <div className='Navbar-2'>
-                <a href="http://localhost:3000/">
-                    <img src={logoNavbar} style={{ height: '7vh' }} />
-                </a>
-            </div>
 
-            {/* <div className='Navbar-2-1'> */}
+
             {!isLoggedIn && (
-                <div className='Navbar-2-1'>
-                    <>
+                <div className='Navbar-1'>
+
+                    <div className='Navbar-2-1'>
+                        <a href="http://localhost:3000/">
+                            <img src={logoNavbar} />
+                        </a>
+                    </div>
+
+
+                    <div className='Navbar-2-2'>
                         <p>Already hadve an account?</p>
-                    </>
-                    <>
+                    </div>
+
+                    <div className='Navbar-2-3'>
                         <Link to="/login"> <button style={{
                             backgroundColor: 'Transparent',
                             border: '1.5px solid black',
@@ -33,41 +37,66 @@ function Navbar() {
                             paddingLeft: '14px',
                             paddingRight: '14px',
                         }}>LOGIN</button> </Link>
-                    </>
+                    </div>
                 </div>
             )}
-            {/* </div> */}
 
 
             {/* 在登入狀態中 可以前往查看的連結 */}
-            <div className='Navbar-2-2'>
-                {isLoggedIn && (
-                    <div className='Navbar-3'>
+
+            {isLoggedIn && (
+
+                <div className='Navbar-1'>
+                    <div className='Navbar-2-1'>
+                        <a href="http://localhost:3000/">
+                            <img src={logoNavbar} style={{ height: '7vh' }} />
+                        </a>
+                    </div>
+
+                    <div className='Navbar-2-4'>
+                        <img
+                            src="https://res.cloudinary.com/dsy2gebem/image/upload/v1658575063/third-project-bg/iconwalk_bpncwm.png"
+                            style={{ height: '3.5vh' }} />
+
+                    </div>
+
+
+                    <div className='Navbar-2-2'>
+
                         <Link to="/users">
                             <button style={{
-                                backgroundColor: 'rgba(255,255,102,0.8)',
+                                // backgroundColor: 'rgba(255,255,102,0.8)',
+                                backgroundColor: 'transparent',
+
                                 border: '1px solid yellow',
-                                padding: '7.2px 25px',
+                                border:'none',
+                                padding: '8px 27px 8px 27px',
                                 // borderRadius: '3px',
                                 color: 'black',
                                 fontWeight: 'bold',
-                                letterSpacing: '0.6px',
+                                letterSpacing: '-0.3px',
+                                fontSize:'1.2rem',
 
                                 marginRight: '30px'
-                            }}>WALKING AROUND</button>
-                        </Link>
+                            }}>WALK AROUND</button>
 
+
+
+                        </Link>
+                    </div>
+                    <div className='Navbar-2-3'>
                         <button style={{
                             backgroundColor: 'Transparent',
                             border: '1px solid gray',
                             padding: '7px',
-                            // borderRadius: '3px',
+                            borderRadius: '3px',
                             paddingLeft: '14px',
                             paddingRight: '14px'
                         }} onClick={logOutUser}>LOG OUT</button>
                     </div>
-                )}
-            </div>
+                </div>
+            )}
+
 
         </nav >
     );
