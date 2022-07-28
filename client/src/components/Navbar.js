e24623ee9e4e1e2e83f85b884264e272984cc77d
WHL99 <xjv298347@gmail.com>
2022-07-28 12:36:03 +0200

    delete URL

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -66,7 +66,7 @@ function Navbar() {
 
                 <div className='Navbar-1'>
                     <div className='Navbar-2-1'>
-                        <a href="http://localhost:3000/">
+                        <a href="/">
                             <img src={logoNavbar} style={{ height: '7vh' }} />
                         </a>
                     </div>
